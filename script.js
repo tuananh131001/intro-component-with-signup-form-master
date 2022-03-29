f61de3f4c63cfff041e039be441029723c77e8af
William <s3864077@rmit.edu.vn>
2022-03-29 20:27:07 +0700

    good

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const inputFields = document.querySelectorAll("input");
-const submitForm = document.querySelector("button");
+const submitForm = document.querySelector(".form-container");
 const errorNotis = document.querySelectorAll(".form_error");
+const email = document.querySelector(".email");
+function formValidation(event) {
+  for (let i = 0; i < 4; i++) {
+    inputFields[i].classList.remove("error-icon");
+    errorNotis[i].style.display = "none";
 
-const validateForm = () => {
-  errorNotis.forEach((errorNoti) => errorNoti.style.display = "block");
-};
+    if (!email.validity.valid) {
+      errorNotis[2].style.display = "block";
+      email.classList.add("error-icon");
+      event.preventDefault();
+    }
+    if (
+      (inputFields.type != "email" || inputFields.type != "submit") &&
+      inputFields[i].value == ""
+    ) {
+      errorNotis[i].style.display = "block";
+      inputFields[i].classList.add("error-icon");
+      event.preventDefault();
+    }
+  }
+}
 
-submitForm.addEventListener("click", validateForm);
+submitForm.addEventListener("submit", formValidation);
